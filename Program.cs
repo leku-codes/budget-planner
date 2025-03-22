03b410bd53413181e02e97522aba2f2f6a1a1c0e
Leon Küssner <leon.kuessner@hrm-systems.ch>
2025-03-22 16:01:47 +0100

    make code more readable

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
-﻿internal class Program
+﻿namespace BudgetPlanning;
+
+internal class Program
 {
-    // TODO filters in listbalance syntax list balance sort_by="value" sort="highest", list balance sort_by="value" sort="lowest"
+    // TODO filters in show_transactions sort_by="value" sort="highest", show_transactions sort_by="value" sort="lowest"
     // TODO filter by category
     // TODO filter by date - last month, last week, last year
     // TODO filter by amount less / most
@@ -10,24 +12,9 @@
     // TODO add a way to edit transactions
     // TODO add a way to save transactions to a file
     // TODO add a way to load transactions from a file
-    
-    private static readonly List<Transaction> TransactionsList = new();
 
-    private static void IntroductionMessage()
-    {
-        Console.WriteLine();
-        Console.WriteLine("Welcome to the budgeting program! Type 'exit' to quit or 'help' for a list of all commands.");   
-    }
+    private static readonly List<Transaction> TransactionsList = [];
     
-    private static void CommandOverview()
-    {
-        Console.WriteLine("expense add <Amount> <Category> <Description>");
-        Console.WriteLine("income add <Amount> <Category> <Description>");
-        Console.WriteLine("show_transactions - to display all transactions");
-        Console.WriteLine("balance - to show your current balance");
-        Console.WriteLine("clear - to clear the screen");
-    }
-
     private static void Main()
     {
         IntroductionMessage();
@@ -39,40 +26,55 @@
             HandleCommand(command);
         }
     }
-    
+
     private static void HandleCommand(string? command)
     {
-        string? trimmedCommand = command?.Trim().ToLower();
-        string[]? commandParts = trimmedCommand?.Split(' ');
+        if (string.IsNullOrWhiteSpace(command))
+        {
+            return;
+        }
 
-        if (commandParts?.Contains("exit") == true) Environment.Exit(0);
-        if (commandParts?.Contains("help") == true)
+        string trimmedCommand = command.Trim().ToLower();
+        string[] commandParts = trimmedCommand.Split(' ');
+
+        bool isExitCommand = commandParts.Contains("exit");
+        bool isHelpCommand = commandParts.Contains("help");
+        bool isShowTransactionsCommand = commandParts.Contains("show_transactions");
+        bool isClearCommand = commandParts.Contains("clear");
+        bool isBalanceCommand = commandParts.Contains("balance");
+        bool isExpenseCommand = commandParts.Contains("expense");
+        bool isIncomeCommand = commandParts.Contains("income");
+        bool isAddCommand = commandParts.Contains("add");
+        bool isSortByCommand = commandParts.Contains("sort_by");
+
+        if (isExitCommand) Environment.Exit(0);
+        if (isHelpCommand)
         {
             CommandOverview();
-        };
+        }
 
-        if (commandParts?.Contains("clear") == true)
+        if (isClearCommand)
         {
             Console.Clear();
             IntroductionMessage();
         }
 
-        if (commandParts?.Contains("show_transactions") == true)
+        if (isShowTransactionsCommand)
         {
             ShowTransactionList();
         }
 
-        if (commandParts?.Contains("balance") == true)
+        if (isBalanceCommand)
         {
             ShowBalance();
         }
 
-        bool hasOptionalDescription = commandParts?.Length == 5;
-        bool hasAmountAndCategory = commandParts?.Length == 4;
-        bool isCategoryMissing = commandParts?.Length == 3;
-        bool isAmountMissing = commandParts?.Length == 2;
-        
-        if (commandParts?.Contains("expense") == true && commandParts.Contains("add"))
+        bool hasOptionalDescription = commandParts.Length == 5;
+        bool hasAmountAndCategory = commandParts.Length == 4;
+        bool isCategoryMissing = commandParts.Length == 3;
+        bool isAmountMissing = commandParts.Length == 2;
+
+        if (isExpenseCommand && isAddCommand)
         {
             if (hasOptionalDescription)
             {
@@ -81,23 +83,24 @@
                 string description = commandParts[4];
                 AddExpense(decimal.Parse(amount), category, description);
             }
-            if (hasAmountAndCategory)
+            else if (hasAmountAndCategory)
             {
                 string amount = commandParts[2];
                 string category = commandParts[3];
                 AddExpense(decimal.Parse(amount), category);
             }
-            else if(isCategoryMissing)
+            else if (isCategoryMissing)
             {
                 Console.WriteLine("Please enter a category for the expense.");
-            } 
+            }
             else if (isAmountMissing)
             {
                 Console.WriteLine("Please enter an amount for the expense.");
             }
         }
-        
-        if (!(commandParts?.Contains("income") == true && commandParts?.Contains("add") == true))
+
+        bool isIncomeAdd = isIncomeCommand && isAddCommand;
+        if (!isIncomeAdd)
         {
             return;
         }
@@ -105,11 +108,11 @@
         if (isAmountMissing)
         {
             Console.WriteLine("Please enter an amount for the income.");
-        } 
-        else if(isCategoryMissing)
+        }
+        else if (isCategoryMissing)
         {
             Console.WriteLine("Please enter a category for the income.");
-        } 
+        }
         else
         {
             string amount = commandParts[2];
@@ -122,24 +125,27 @@
     {
         AddExpense(amount, category, "");
     }
-    
+
     private static void AddExpense(decimal amount, string category, string description)
     {
-        var transaction = new Transaction(-amount, category, description);
+        Transaction transaction = new Transaction(-amount, category, description);
         TransactionsList.Add(transaction);
-        string outputMessage = $"Expense added: {amount}kr for {category}";
-        if (!string.IsNullOrEmpty(description))
-        {
-            outputMessage += $" with description: {description}";
-        }
-        Console.WriteLine(outputMessage);
+        LineSeparatedHeadline("Added new expense record:");
+        TableLogSetup();
+        NewTransactionLog(transaction);
     }
 
     private static void AddIncome(decimal amount, string category)
     {
-        var transaction = new Transaction(amount, category);
+        AddIncome(amount, category, "");
+    }
+
+    private static void AddIncome(decimal amount, string category, string description)
+    {
+        Transaction transaction = new Transaction(-amount, category, description);
         TransactionsList.Add(transaction);
-        Console.WriteLine($"Income added: {amount}kr for {category}");
+        TableLogSetup();
+        NewTransactionLog(transaction);
     }
 
     private static void ShowBalance()
@@ -166,14 +172,56 @@
             Console.WriteLine("There are no transactions to show.");
         }
         else
-        { 
-            Console.WriteLine(
-                $"{"Date",-12} | {"Time",-8} | {"Amount",-12} | {"Category",-15} | {"Description",-30}");
-            Console.WriteLine(new string('-', 85));
-            for (var i = 0; i < TransactionsList.Count; i++){
-                Console.WriteLine(
-                    $"{TransactionsList[i].Date,-12} | {TransactionsList[i].Time,-8} | {TransactionsList[i].Amount,-10:F2}kr | {TransactionsList[i].Category,-15} | {TransactionsList[i].Description,-30}");
+        {
+            TableLogSetup();
+            for (var i = 0; i < TransactionsList.Count; i++)
+            {
+                Console.WriteLine($"{TransactionsList[i].Id,-6} | {TransactionsList[i].Date,-12} | {TransactionsList[i].Time,-8} | {TransactionsList[i].Amount,-10:F2}kr | {TransactionsList[i].Category,-15} | {TransactionsList[i].Description,-30}");
             }
+            Console.WriteLine();
         }
     }
+
+    private static void TableLogSetup()
+    {
+        string tableCols = $"{"ID",-6} | {"Date",-12} | {"Time",-8} | {"Amount",-12} | {"Category",-15} | {"Description",-30}";
+        string dashedSeparator = new string('-', 85);
+        Console.WriteLine(tableCols);
+        Console.WriteLine(dashedSeparator);
+    }
+
+    private static void LineSeparatedHeadline(string headline)
+    {
+        Console.WriteLine();
+        Console.WriteLine(headline);
+        Console.WriteLine();
+    }
+    
+    private static void NewTransactionLog(Transaction transaction)
+    {
+        string outputMessage = $"{transaction.Id,-6} | {transaction.Date,-12} | {transaction.Time,-8} | {transaction.Amount,-10:F2}kr | {transaction.Category,-15}";
+        if (!string.IsNullOrEmpty(transaction.Description))
+        {
+            outputMessage +=
+                $" | {transaction.Description,-30}";
+        }
+
+        Console.WriteLine(outputMessage);
+    }
+    
+    private static void IntroductionMessage()
+    {
+        Console.WriteLine();
+        Console.WriteLine("Welcome to the budgeting program! Type 'exit' to quit or 'help' for a list of all commands.");
+    }
+
+    private static void CommandOverview()
+    {
+        Console.WriteLine("expense add <Amount> <Category> <Description>");
+        Console.WriteLine("income add <Amount> <Category> <Description>");
+        Console.WriteLine("show_transactions - to display all transactions");
+        Console.WriteLine("balance - to show your current balance");
+        Console.WriteLine("clear - to clear the screen");
+    }
+
 }
--- Transactions.cs
+++ Transactions.cs
@@ -1,12 +1,10 @@
-using System;
-using System.Collections.Generic;
-
 public struct Transaction
 {
-    private static int Id = 0;
-    public decimal Amount;
-    public string Category;
-    public string Description;
+    private static int _nextId = 1;
+    public readonly int Id;
+    public readonly decimal Amount;
+    public readonly string Category;
+    public readonly string Description;
     public DateOnly Date;
     public TimeOnly Time;
     
@@ -16,16 +14,11 @@ public struct Transaction
     
     public Transaction(decimal amount, string category, string description)
     {
-        Id = generateId();
+        Id = _nextId++;
         Date = DateOnly.FromDateTime(DateTime.Now);
         Time = TimeOnly.FromDateTime(DateTime.Now);
         Amount = amount;
         Category = category;
         Description = description;
-    }
-    
-    static int generateId()
-    {
-        return Id++;
     }
 }
